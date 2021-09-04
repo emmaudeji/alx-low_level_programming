1902a9117430ee62d2c895f1bbe68c839febe3c0
emmaudeji <gogreneinfo@gmail.com>
2021-09-04 08:25:42 -0700

    checks for uppercase

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,33 @@ int _putchar(char);
 /*_isupper.c*/
 int _isupper(int c);
 
+/* _isdigit.c*/
+int _isdigit(int c);
+
+/*2-mal.c*/
+int mul(int a, int b);
+
+/*3-print_numbers.c*/
+void print_numbers(void);
+
+/*4-print_most_numbers.c*/
+void print_most_numbers(void);
+
+/*5-maor_numbers.c*/
+void more_numbers(void);
+
+/*6-print_line.c*/
+void print_line(int n);
+
+/*7-print_diagonal.c*/
+void print_diagonal(int n);
+
+/*8-print_square.c*/
+void print_square(int size);
+
+/*10-print_triangle.c*/
+void print_triangle(int size);
+
+/*101-print_number.c*/
+void print_number(int n);
 #endif
