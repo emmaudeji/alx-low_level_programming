7ad06ae6529ae66232b16cbd36cd53d2d99d7a94
emmaudeji <gogreneinfo@gmail.com>
2021-09-30 01:52:36 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
 
-
 /**
- * print_name - will print a name
- * @name: is a string
- * @f: function pointer
+ * print_name - prints a name
+ * @name: string
+ * @f: function
  */
 
 void print_name(char *name, void (*f)(char *))
- {
-if(f != NULL)
-    	f(nane);
- }
+{
+if (f != NULL)
+	f(name);
+}
