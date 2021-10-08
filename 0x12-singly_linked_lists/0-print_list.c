b9de5c231ba3247b11d3f97b5621de01392f07dc
emmaudeji <gogreneinfo@gmail.com>
2021-10-08 15:13:58 -0700

    prints element of a string

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,14 +4,13 @@
 /**
  * print_list - print elements of linkedlist
  * @h: linked list
- * @count: number of nodes
- * 
+ *
  * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count_nodes = 0;
+	size_t nbr_nodes = 0;
 	const list_t *current = h;
 
 	while (current != NULL)
@@ -19,7 +18,7 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", current->len,
 		current->str != NULL ? current->str : "(nil)");
 		current = current->next;
-		count_nodes++;
+		nbr_nodes++;
 	}
-	return (count_nodes);
+	return (nbr_nodes);
 }
