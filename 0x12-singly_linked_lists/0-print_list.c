bdc7d311dc50d13eb242aa6ed6e0a88c36888887
emmaudeji <gogreneinfo@gmail.com>
2021-10-08 15:09:14 -0700

    updating

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", current->len,
 		current->str != NULL ? current->str : "(nil)");
 		current = current->next;
-		nbr_nodes++;
+		count_nodes++;
 	}
 	return (count_nodes);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdlib.h>
 /**
@@ -9,9 +9,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -20,13 +19,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-
 size_t list_len(const list_t *h);
-
 list_t *add_node(list_t **head, const char *str);
-
 list_t *add_node_end(list_t **head, const char *str);
-
 void free_list(list_t *head);
 
 
