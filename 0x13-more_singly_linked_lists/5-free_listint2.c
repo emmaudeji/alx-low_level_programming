4790073d8afce11d5c63dba9a92ea907abd8c7b2
emmaudeji <gogreneinfo@gmail.com>
2021-10-12 12:25:43 -0700

    free nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,26 @@
 #include "lists.h"
+
 /**
- *free_listint - The function that free a memory location
- *@head: the head of the linked list
+ * free_listint2 - frees a listint_t list
+ * Description: at the end, the head will point to NULL.
+ * So, as we know the tail always points to NULL, we will
+ * be moving a temp pointer, free its memory if not NULL
+ * Untill we reach the tail
+ * @head: head of linked list
  */
 
 void free_listint2(listint_t **head)
-
 {
-	listint_t *tmp;
+	listint_t *current;
 
 	if (head == NULL)
-
 		return;
 
-	while (head)
-
+	while (*head != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		current = *head;
+		*head = (*head)->next;
+		free(current);
 	}
+
 }
