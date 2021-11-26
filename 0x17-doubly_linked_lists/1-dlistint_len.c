d26dc6b43184308a57971658f6cd06b3a151f6bb
emmaudeji <gogreneinfo@gmail.com>
2021-11-26 08:47:13 -0800

    print numbr of elements in list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,17 +2,13 @@
 
 /**
  * dlistint_len: checks the number of elements in the list
- *
- * @head: pointer to the head of the linked list
+ * @h: pointer to the head of the linked list
  *
  * Return: the number of elements in the dlistint_t
- * Description: count the elements by counting the nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	
 	size_t count_nodes = 0;
-
 	
 	while (h)
 	{
