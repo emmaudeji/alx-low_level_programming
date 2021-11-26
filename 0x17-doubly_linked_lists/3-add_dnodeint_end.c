9416089c181ab288b962839f71e7973f86bc320b
emmaudeji <gogreneinfo@gmail.com>
2021-11-26 09:55:36 -0800

    ads node to the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,38 +2,36 @@
 
 /**
  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: pointer to the head of the dlistint_t list.
- * @n: data for the new node to contain.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
  *
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
- dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
- {
-     dlistint_t *new, *tail;
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
 
-     new = malloc(sizeof(dlistint_t));
-     if (new == NULL)
-     {
-	     return (NULL);
-     }
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-     new->n = n;
-     new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-     if (*head == NULL)
-     {
-         new->prev = NULL;
-         *head = new;
-         return (new);
-     }
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 
-     tail = *head;
-     while (tail->next != NULL)
-         tail = tail->next;
-     tail->next = new;
-     new->prev = tail;
-     tail = new;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+	last = new;
 
-     return (tail);
- }
+	return (last);
+}
