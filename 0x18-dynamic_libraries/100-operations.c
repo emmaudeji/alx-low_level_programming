cbc20f9c2f5641a1e36f70e91a0ce0d5fb5b06bc
emmaudeji <gogreneinfo@gmail.com>
2021-12-02 18:59:15 -0800

    100-operations.c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - subtract
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - multiply
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - division
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - 
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
